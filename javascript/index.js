d41b0aaa87b1cd77c7ef6ab2004edc4752dcbf46
Berwin <berwinsjtu@gmail.com>
2013-12-06 12:52:00 +0800

    add log out button, change link of ticket

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	$('#head_submit').click(function(){
 		$('#head_login').fadeOut(1);
-		$('#head_menu').append("<p>aaa</p>");
+		$('#head_menu').append("<p style='float:left' id='user_info'>aaa</p>");
+		$('#head_menu').append("<button type='submit' class='btn' id='logout'>退出</button>");
+	});
+	$('body').on("click","#logout",function(){
+		$('#logout').remove();
+		$('#user_info').remove();
+		$('#head_login').fadeIn(1);
 	});
 });
